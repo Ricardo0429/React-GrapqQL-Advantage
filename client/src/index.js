16d80e8266cf319f9462d6c4f05fe53e654c9b01
Ricardo <121136503+Ricardo0429@users.noreply.github.com>
2023-02-27 21:25:09 -0500

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Auth from './Auth';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
